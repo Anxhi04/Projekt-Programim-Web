89fcf800b6850be57e053e88e2d73f2a03098e9b
Anxhelika <krecianxhelika04@gmail.com>
2025-12-24 03:23:52 +0100

    shtimi i navbarit dhe stilizimi i tij

---
+++ projekt/includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+     <head>
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+         <link href="css\nav.css" rel="stylesheet">
+         <meta charset="utf-8">
+         <meta content="width=device-width, initial-scale=1.0" name="viewport">
+     </head>
+     <body>
+     <ul class="nav nav-pills mb-3 shadow" id="pills-tab" role="tablist">
+         <li class="nav-item" role="presentation">
+             <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Beauty Center</button>
+         </li>
+         <li class="nav-item" role="presentation">
+             <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Home🏚️</button>
+         </li>
+         <li class="nav-item" role="presentation">
+             <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Book Service📆</button>
+         </li>
+         <li class="nav-item ms-auto" >
+             <button class="nav-link active login" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Log in</button>
+         </li>
+     </ul>
+
+     </body>
+
+</html>
