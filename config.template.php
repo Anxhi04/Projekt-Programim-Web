22529f91f6eef428c2452166198a3af921c1b556
Emada Nipolli <emadanipolli@gmail.com>
2025-12-30 01:19:24 +0100

    db config template

---
+++ config.template.php
@@ -0,0 +1,11 @@
+<?php
+
+//kjo eshte template
+[
+    'db_host' => 'localhost-zakonisht',
+    'db_port' => 'nrPortes',
+    'db_name' => 'emriSkemesQeKe',
+    'db_user' => 'emriUserit',
+    'db_pass' => '',
+];
+?>
