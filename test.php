7dd32825688ef542eeefb431b35b2afacd7d8793
Emada Nipolli <emadanipolli@gmail.com>
2026-01-04 02:46:52 +0100

    pjesa e pare e verifikimit te accountit me email

---
+++ test.php
@@ -0,0 +1,47 @@
+<?php
+echo "<h1>Send E-mail</h1>";
+
+use PHPMailer\PHPMailer\PHPMailer\src\Exception;
+use PHPMailer\PHPMailer\PHPMailer\src\PHPMailer;
+
+require 'PHPMailer/PHPMailer/src/Exception.php';
+require 'PHPMailer/PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = false;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'verifyGlamBook@gmail.com';                     //SMTP username
+    $mail->Password = 'vogp shwt leod euox';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('verifyGlamBook@gmail.com', 'No reply');
+    $mail->addAddress('emadanipolli@gmail.com', 'Emada');     //Add a recipient
+    $mail->addAddress('adresaJuaj@gmail.com');          //vendosni emailin tuaj per ta provuar     //Name is optional
+//    $mail->addReplyTo('info@example.com', 'Information');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Verify your email address';
+    $mail->Body = 'This is your pass: <b>2345678990</b>';
+//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
